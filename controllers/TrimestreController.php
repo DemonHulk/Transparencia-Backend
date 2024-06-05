ed79e490323a40a851b8292d7654c3dc15c39d1d
x1Wander <162844921+x1Wander@users.noreply.github.com>
2024-06-04 19:40:24 -0700

    Backend crud trimestre (#3)
    * Crud's para el trimestre
    
    se realizaron los crud's correspondientes para trimestre
    
    * Archivo de conexion recuperado (Parche)
    
    Despues de ciertos bugs se arreglo
    
    ---------
    
    Co-authored-by: Marco Fabian <al05-016-0321@utdelacosta.edu.mx>

--- controllers/TrimestreController.php
+++ controllers/TrimestreController.php
@@ -11,4 +11,49 @@ class TrimestreController {
         $this->TrimestreModel = new TrimestreModel();
     }
 
+    public function QueryAllController() {
+        try {
+            $resultado = $this->TrimestreModel->QueryAllModel();
+            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+
+    public function QueryOneController($id) {
+        try {
+            $resultado = $this->TrimestreModel->QueryOneModel($id);
+            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+
+    public function InsertController($datos) {
+        try {
+            $resultado = $this->TrimestreModel->InsertModel($datos);
+            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+
+    public function UpdateController($id, $datos) {
+        try {
+            $resultado = $this->TrimestreModel->UpdateModel($id, $datos);
+            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+
+    public function DeleteController($id) {
+        try {
+            $resultado = $this->TrimestreModel->DeleteModel($id);
+            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+
 }
