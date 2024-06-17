e9d746f6171d25194b782a1742c28a8222d966a0
x1Wander <xwanderx748@gmail.com>
2024-06-16 23:59:47 -0700

    crud's de trimestre
    se implemetarón los crud's para guardar, editar y mostrar los datos de trimestre.

--- controllers/TrimestreController.php
+++ controllers/TrimestreController.php
@@ -2,19 +2,29 @@
 
 require_once 'models/TrimestreModel.php';
 require_once 'middleware/ExceptionHandler.php';
+require_once 'models/EncryptModel.php';
+require_once 'models/ValidacionesModel.php';
 
 class TrimestreController {
 
     private $TrimestreModel;
+    private $validacionesModel;
+    private $EncryptModel;
 
     public function __construct() {
         $this->TrimestreModel = new TrimestreModel();
+        $this->validacionesModel = new ValidacionesModel();
+        $this->EncryptModel = new EncryptModel();
     }
 
     public function QueryAllController() {
         try {
             $resultado = $this->TrimestreModel->QueryAllModel();
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            $response =  json_encode(['estado' => 200, 'resultado' => ['res' => true, 'data' => $resultado]]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
@@ -22,35 +32,155 @@ class TrimestreController {
 
     public function QueryOneController($id) {
         try {
-            $resultado = $this->TrimestreModel->QueryOneModel($id);
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            $decryptedID = $this->EncryptModel->decryptData($id);
+            $resultado = $this->TrimestreModel->QueryOneModel($decryptedID);
+            $response = json_encode(['estado' => 200, 'resultado' => $resultado]);
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
     }
 
     public function InsertController($datos) {
+        // Obtener los datos encriptados
+        $encryptedData = $datos['data'];
+    
         try {
-            $resultado = $this->TrimestreModel->InsertModel($datos);
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos encriptados a la funcion para desencriptarlos
+            $decryptedData = $this->EncryptModel->decryptData($encryptedData);
+        } catch (Exception $e) {
+            $response = json_encode(['estado' => 200, 'resultado' => ['res' => false, 'data' => $e->getMessage()]]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+            return;
+        }
+         // Asignar fechas y horas
+        $decryptedData['fecha_creacion'] = date('Y-m-d');
+        $decryptedData['hora_creacion'] = date('H:i:s');
+        $decryptedData['fecha_actualizado'] = date('Y-m-d');
+        $decryptedData['activo'] = true;
+        try {
+            // Llamamos al método InsertModel y guardamos el resultado
+            list($success, $message) = $this->TrimestreModel->InsertModel($decryptedData);
+            
+            // Si la inserción fue exitosa, preparamos una respuesta con estado 200
+            if ($success) {
+                $response = json_encode(['estado' => 200, 'resultado' => ['res' => true, 'data' => $message]]);
+            } else {
+                // Si hubo un error (Trimestre duplicado), preparamos una respuesta con estado 400 o el que sea adecuado
+                $response = json_encode(['estado' => 400, 'resultado' => ['res' => false, 'data' => $message]]);
+            }
+
+            // Encriptamos la respuesta
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
     }
 
     public function UpdateController($id, $datos) {
+
+        // Obtener los datos encriptados
+        $encryptedData = $datos['data'];
+
         try {
-            $resultado = $this->TrimestreModel->UpdateModel($id, $datos);
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos encriptados a la funcion para desencriptarlos
+            $decryptedData = $this->EncryptModel->decryptData($encryptedData);
+            $decryptedID = $this->EncryptModel->decryptData($id);
+        } catch (Exception $e) {
+            echo json_encode(['estado' => 200, 'resultado' => $e->getMessage()]);
+            return;
+        }
+
+        // Validar los datos desencriptados
+        if (!isset($decryptedData['trimestre']) || !$this->validacionesModel->ValidarTextoNumero($decryptedData['trimestre'])) {
+            $response = json_encode(['estado' => 200, 'resultado' => ['res' => false, 'data' => "El nombre del trimestre no es válido."]]);
+             // Mandamos los datos a encriptar
+             $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+             // Retornamos los datos ya encriptados
+             echo $encryptedResponse;
+            return;
+        }
+    
+        // Asignar fechas y horas
+        $decryptedData['fecha_creacion'] = date('Y-m-d');
+        $decryptedData['hora_creacion'] = date('H:i:s');
+        $decryptedData['fecha_actualizado'] = date('Y-m-d');
+    
+        // Validar fechas y horas
+        if (!$this->validacionesModel->ValidarFecha($decryptedData['fecha_creacion']) ||
+            !$this->validacionesModel->ValidarHora($decryptedData['hora_creacion']) ||
+            !$this->validacionesModel->ValidarFecha($decryptedData['fecha_actualizado'])) {
+            $response = json_encode(['estado' => 200, 'resultado' => ['res' => false, 'data' => "Las fechas u horas no son válidas."]]);
+             // Mandamos los datos a encriptar
+             $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+             // Retornamos los datos ya encriptados
+             echo $encryptedResponse;
+            return;
+        }
+    
+        try {
+            // Actualizar usuario
+            $resultado = $this->TrimestreModel->UpdateModel($decryptedID, $decryptedData);
+            $response = json_encode(['estado' => 200, 'resultado' => $resultado]);
+             // Mandamos los datos a encriptar
+             $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+             // Retornamos los datos ya encriptados
+             echo $encryptedResponse;
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
     }
 
     public function DeleteController($id) {
+        // Asignar fecha actualizada
+        $datos['fecha_actualizado'] = date('Y-m-d');
+
+        // Validar fecha actualizada
+        if (!$this->validacionesModel->ValidarFecha($datos['fecha_actualizado'])) {
+            echo json_encode(['estado' => 200, 'resultado' => ['res' => false, 'data' => "La fecha actualizada no es válida."]]);
+            return;
+        }
         try {
-            $resultado = $this->TrimestreModel->DeleteModel($id);
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            $decryptedID = $this->EncryptModel->decryptData($id);
+            // Actualizamos el estado
+            $resultado = $this->TrimestreModel->DeleteModel($decryptedID, $datos);
+            $response = json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+    
+    public function ActivateController($id) {
+        // Asignar fecha actualizada
+        $datos['fecha_actualizado'] = date('Y-m-d');
+
+        // Validar fecha actualizada
+        if (!$this->validacionesModel->ValidarFecha($datos['fecha_actualizado'])) {
+            echo json_encode(['estado' => 200, 'resultado' => ['res' => false, 'data' => "La fecha actualizada no es válida."]]);
+            return;
+        }
+        try {
+            $decryptedID = $this->EncryptModel->decryptData($id);
+            // Actualizamos el estado
+            $resultado = $this->TrimestreModel->ActivateModel($decryptedID, $datos);
+            $response = json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
