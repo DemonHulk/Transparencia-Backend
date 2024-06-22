31ff5c328b4e8f85b0eef99023e8587ba768b6ed
Ernesto Ibarra Villanueva <al05-011-0321@utdelacosta.edu.mx>
2024-06-21 22:56:30 -0700

    Crud titulos de prueba
    Se creo el crud en tabla, insert, update, delete y reactive, cuenta con un fallo aun al activar y desactivar que ya no deja mover la tabla

--- controllers/TitulosController.php
+++ controllers/TitulosController.php
@@ -3,17 +3,19 @@
 require_once 'models/TitulosModel.php';
 require_once 'middleware/ExceptionHandler.php';
 require_once 'models/ValidacionesModel.php';
-
+require_once 'models/EncryptModel.php';
 
 
 class TitulosController {
 
     private $TitulosModel;
     private $validacionesModel;
+    private $EncryptModel;
 
     public function __construct() {
         $this->TitulosModel = new TitulosModel();
         $this->validacionesModel = new ValidacionesModel();
+        $this->EncryptModel = new EncryptModel(); 
     }
 
     public function QueryAllController() {
@@ -34,28 +36,157 @@ class TitulosController {
         }
     }
 
+    public function QueryTitulosPuntoController($id) {
+        try {
+            $decryptedID = $this->EncryptModel->decryptData($id);
+            $resultado = $this->TitulosModel->QueryTitulosPuntoModel($decryptedID);
+            $response = json_encode(['estado' => 200, 'resultado' =>  $resultado]);
+
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+    /**
+     * extrae los datos de un titulo + nombre del punto a que pertenece e id
+     */
+    public function QueryTitulosMasPuntoController($id) {
+        try {
+            $decryptedID = $this->EncryptModel->decryptData($id);
+            $resultado = $this->TitulosModel->QueryTitulosMasPuntoModel($decryptedID);
+            $response = json_encode(['estado' => 200, 'resultado' =>  $resultado]);
+
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+
+
+    /**
+     * Insert para un titulo
+     */
     public function InsertController($datos) {
+         // Obtener los datos encriptados
+         $encryptedData = $datos['data'];
         try {
-            $resultado = $this->TitulosModel->InsertModel($datos);
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos encriptados a la funcion para desencriptarlos
+            $decryptedData = $this->EncryptModel->decryptData($encryptedData);
+        } catch (Exception $e) {
+            $response = json_encode(['estado' => 200, 'resultado' => ['res' => false, 'data' => $e->getMessage()]]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+            return;
+        }
+
+        // Asignar fechas y horas
+        $decryptedData['fecha_creacion'] = date('Y-m-d');
+        $decryptedData['hora_creacion'] = date('H:i:s');
+        $decryptedData['fecha_actualizado'] = date('Y-m-d');
+
+
+        try {
+            // Insertar punto
+            $resultado = $this->TitulosModel->InsertModel($decryptedData);
+            $response = json_encode(['estado' => 200, 'resultado' =>  $resultado]);
+             // Mandamos los datos a encriptar
+             $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+             // Retornamos los datos ya encriptados
+             echo $encryptedResponse;
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
     }
 
+
+
+
+    /**
+     * Actualiza un titulo existente.
+     * @param int $id ID del titulo.
+     * @param array $datos Datos del titulo.
+     */
     public function UpdateController($id, $datos) {
+        // Obtener los datos encriptados
+        $encryptedData = $datos['data'];
+    
         try {
-            $resultado = $this->TitulosModel->UpdateModel($id, $datos);
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos encriptados a la funcion para desencriptarlos
+            $decryptedData = $this->EncryptModel->decryptData($encryptedData);
+            $decryptedID = $this->EncryptModel->decryptData($id);
+        } catch (Exception $e) {
+            $response = json_encode(['estado' => 200, 'resultado' => ['res' => false, 'data' => $e->getMessage()]]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+            return;
+        }
+
+        // Asignar fecha actualizada
+        $decryptedData['fecha_actualizado'] = date('Y-m-d');
+
+        try {
+            // Actualizar titulo
+            $resultado = $this->TitulosModel->UpdateModel($decryptedID, $decryptedData);
+            $response = json_encode(['estado' => 200, 'resultado' =>  $resultado]);
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
     }
 
+
+    /**
+     * Desactiva un titulo.
+     * @param int $id ID del titulo.
+     */
     public function DeleteController($id) {
+        // Asignar fecha actualizada
+        $datos['fecha_actualizado'] = date('Y-m-d');
+
         try {
-            $resultado = $this->TitulosModel->DeleteModel($id);
-            echo json_encode(['estado' => 200, 'resultado' => $resultado]);
+            $decryptedID = $this->EncryptModel->decryptData($id);
+            // Desactivar área
+            $resultado = $this->TitulosModel->DeleteModel($decryptedID, $datos);
+            $response = json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
+        } catch (Exception $e) {
+            ExceptionHandler::handle($e);
+        }
+    }
+
+
+    /**
+     * Activa un titulo.
+     * @param int $id ID del titulo.
+     */
+    public function ActivateController($id) {
+        // Asignar fecha actualizada
+        $datos['fecha_actualizado'] = date('Y-m-d');
+
+        try {
+            $decryptedID = $this->EncryptModel->decryptData($id);
+            // Activar titulo
+            $resultado = $this->TitulosModel->ActivateModel($decryptedID, $datos);
+            $response = json_encode(['estado' => 200, 'resultado' => $resultado]);
+            // Mandamos los datos a encriptar
+            $encryptedResponse = $this->EncryptModel->encryptJSON($response);
+            // Retornamos los datos ya encriptados
+            echo $encryptedResponse;
         } catch (Exception $e) {
             ExceptionHandler::handle($e);
         }
--- models/TitulosModel.php
+++ models/TitulosModel.php
@@ -29,65 +29,230 @@ class TitulosModel {
 
     public function InsertModel($datos) {
         try {
-            $fecha = date('Y-m-d');
-            $hora_creacion = date('H:i:s');
-
             $conn = Conexion::Conexion();
-            $stmt = $conn->prepare("
-                INSERT INTO titulos (nombre_titulo, tipo_contenido, activo, fecha_creacion, hora_creacion, fecha_actualizado) 
-                VALUES (:nombre_titulo, :tipo_contenido, :activo, :fecha_creacion, :hora_creacion, :fecha_actualizado)");
-        
-            // Pasamos los datos que recibimos del frontend y ponemos los datos de la fecha y hora
-            $stmt->bindParam(':nombre_titulo', $datos['nombre_titulo'], PDO::PARAM_STR);
-            $stmt->bindParam(':tipo_contenido', $datos['tipo_contenido'], PDO::PARAM_INT);
-            $stmt->bindParam(':activo', $datos['activo'], PDO::PARAM_BOOL);
-            $stmt->bindParam(':fecha_creacion', $fecha, PDO::PARAM_STR);
-            $stmt->bindParam(':hora_creacion', $hora_creacion, PDO::PARAM_STR);
-            $stmt->bindParam(':fecha_actualizado', $fecha, PDO::PARAM_STR);
+            $conn->beginTransaction();
 
+            // Validar si el nombre del titulo ya existe
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM titulos WHERE nombre_titulo = :nombre and id_punto = :punto and fk_titulos IS NULL");
+            $stmt->bindParam(':nombre', $datos['nombreTitulo'], PDO::PARAM_STR);
+            $stmt->bindParam(':punto', $datos['punto'], PDO::PARAM_INT);
             $stmt->execute();
-            return "Título guardado exitosamente";
+            $existingCount = $stmt->fetchColumn();
+
+            if ($existingCount > 0) {
+                $conn->rollBack();
+                return ['res' => false, 'data' => "Ya existe el titulo con ese nombre"];
+            }
+
+            if (!isset($datos['orden_titulos']) || empty($datos['orden_titulos'])) {
+                // Obtener el último orden existente sin importar el estado
+                $stmt = $conn->prepare("SELECT MAX(orden_titulos) FROM titulos WHERE id_punto = :punto AND fk_titulos IS NULL");
+                $stmt->bindParam(':punto', $datos['punto'], PDO::PARAM_INT);
+                $stmt->execute();
+                $lastOrder = $stmt->fetchColumn();
+                $datos['orden_titulos'] = $lastOrder + 1;
+            }
+
+            // Ajustar los valores de orden_titulos
+            $this->adjustOrderTitulos($conn, $datos['orden_titulos'], 0, $datos['punto']);
+
+
+            // Insertar el punto en la tabla punto
+            $stmt = $conn->prepare("
+                INSERT INTO titulos (id_punto, nombre_titulo, tipo_contenido, link, punto_destino, orden_titulos, activo, fecha_creacion, hora_creacion, fecha_actualizado) 
+                VALUES(:punto, :nombre, :tipocontenido, :eslink, :puntodestino, :orden, true, :fecha_creacion, :hora_creacion, :fecha_actualizado);");
+            $stmt->bindParam(':punto', $datos['punto'], PDO::PARAM_INT);
+            $stmt->bindParam(':nombre', $datos['nombreTitulo'], PDO::PARAM_STR);
+            $stmt->bindParam(':tipocontenido', $datos['tipoContenido'], PDO::PARAM_STR);
+            $esLinkBool = ($datos['esLink'] == '' ? false : true); // Convertir a booleano
+            $stmt->bindParam(':eslink', $esLinkBool, PDO::PARAM_BOOL);
+            $puntoDestino = isset($datos['puntodestino']) ? $datos['puntodestino'] : null; // Manejar NULL
+            $stmt->bindParam(':puntodestino', $puntoDestino, PDO::PARAM_INT);
+            $stmt->bindParam(':orden', $datos['orden_titulos'], PDO::PARAM_INT);
+            $stmt->bindParam(':fecha_creacion', $datos['fecha_creacion'], PDO::PARAM_STR);
+            $stmt->bindParam(':hora_creacion', $datos['hora_creacion'], PDO::PARAM_STR);
+            $stmt->bindParam(':fecha_actualizado', $datos['fecha_actualizado'], PDO::PARAM_STR);
+            $stmt->execute();
+
+
+            $conn->commit();
+            return ['res' => true, 'data' => "Punto guardado exitosamente"];
         } catch (PDOException $e) {
-            throw new Exception("Error al insertar el título: " . $e->getMessage());
+            $conn->rollBack();
+            throw new Exception("Error al insertar el Titulo: " . $e->getMessage());
         }
     }
+
+
+    public function QueryTitulosPuntoModel($id) {
+        try {
+            $conn = Conexion::Conexion();
+            $stmt = $conn->prepare("SELECT * FROM titulos WHERE id_punto = :punto AND fk_titulos IS NULL order by orden_titulos asc");
+            $stmt->bindParam(':punto', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return ['res' => true, 'data' => $stmt->fetchAll(PDO::FETCH_ASSOC)];
+        } catch (PDOException $e) {
+            return ['res' => false, 'data' => "Error al obtener los titulos del punto"];
+        }
+    }
+
+    public function QueryTitulosMasPuntoModel($id) {
+        try {
+            $conn = Conexion::Conexion();
+            $stmt = $conn->prepare("SELECT t.*, p.nombre_punto, p.id_punto  FROM titulos t inner join punto p on p.id_punto = t.id_punto  WHERE t.id_titulo  = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return ['res' => true, 'data' => $stmt->fetch(PDO::FETCH_ASSOC)];
+        } catch (PDOException $e) {
+            return ['res' => false, 'data' => "Error al obtener los datos del titulo"];
+        }
+    }
+
+
+    // public function UpdateModel($id, $datos) {
+    //     try {
+    //         $conn = Conexion::Conexion();
+    //         $stmt = $conn->prepare("
+    //             UPDATE titulos 
+    //             SET nombre_titulo = :nombre_titulo, tipo_contenido = :tipo_contenido, fecha_actualizado = :fecha_actualizado 
+    //             WHERE id_titulo = :id AND activo = true");
+    //         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    //         $stmt->bindParam(':nombre_titulo', $datos['nombre_titulo'], PDO::PARAM_STR);
+    //         $stmt->bindParam(':tipo_contenido', $datos['tipo_contenido'], PDO::PARAM_INT);
+    //         $stmt->bindParam(':fecha_actualizado', $datos['fecha_actualizado'], PDO::PARAM_STR);
+    //         $stmt->execute();
+    //         if ($stmt->rowCount() == 0) {
+    //             throw new Exception("No se encontró un título activo con ID $id");
+    //         }
+    //         return "Título con ID $id actualizado exitosamente";
+    //     } catch (PDOException $e) {
+    //         throw new Exception("Error al actualizar el título: " . $e->getMessage());
+    //     }
+    // }
 
     public function UpdateModel($id, $datos) {
         try {
             $conn = Conexion::Conexion();
+            $conn->beginTransaction();
+
+            // Validar si el nombre del título ya existe
+            $stmt = $conn->prepare("
+                SELECT COUNT(*) 
+                FROM titulos 
+                WHERE nombre_titulo = :nombre 
+                  AND id_punto = :punto 
+                  AND fk_titulos IS NULL 
+                  AND id_titulo != :id");
+            $stmt->bindParam(':nombre', $datos['nombreTitulo'], PDO::PARAM_STR);
+            $stmt->bindParam(':punto', $datos['punto'], PDO::PARAM_INT);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $count = $stmt->fetchColumn();
+
+            if ($count > 0) {
+                $conn->rollBack();
+                return ['res' => false, 'data' => "Error: Ya existe otro Tema con el mismo nombre"];
+            }
+
+            // Proceder con la actualización si no se encuentra duplicado
             $stmt = $conn->prepare("
                 UPDATE titulos 
-                SET nombre_titulo = :nombre_titulo, tipo_contenido = :tipo_contenido, fecha_actualizado = :fecha_actualizado 
-                WHERE id_titulo = :id AND activo = true");
+                SET 
+                    nombre_titulo = :nombreTitulo, 
+                    tipo_contenido = :tipocontenido, 
+                    link = :eslink, 
+                    fecha_actualizado = :fecha_actualizado 
+                WHERE id_titulo = :id");
+            
+            // Vincular los parámetros correctamente
             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-            $stmt->bindParam(':nombre_titulo', $datos['nombre_titulo'], PDO::PARAM_STR);
-            $stmt->bindParam(':tipo_contenido', $datos['tipo_contenido'], PDO::PARAM_INT);
+            $stmt->bindParam(':nombreTitulo', $datos['nombreTitulo'], PDO::PARAM_STR);
+            $stmt->bindParam(':tipocontenido', $datos['tipoContenido'], PDO::PARAM_STR);
+            
+            // Convertir el valor a booleano y vincularlo correctamente
+            $esLinkBool = filter_var($datos['esLink'], FILTER_VALIDATE_BOOLEAN); // Mejor manejo de booleanos
+            $stmt->bindParam(':eslink', $esLinkBool, PDO::PARAM_BOOL);
+            
             $stmt->bindParam(':fecha_actualizado', $datos['fecha_actualizado'], PDO::PARAM_STR);
             $stmt->execute();
+
             if ($stmt->rowCount() == 0) {
-                throw new Exception("No se encontró un título activo con ID $id");
+                $conn->rollBack();
+                return ['res' => false, 'data' => "Error al actualizar el Tema"];
             }
-            return "Título con ID $id actualizado exitosamente";
+
+            $conn->commit();
+            return ['res' => true, 'data' => "Tema actualizado exitosamente"];
         } catch (PDOException $e) {
-            throw new Exception("Error al actualizar el título: " . $e->getMessage());
+            $conn->rollBack();
+            return ['res' => false, 'data' => "Error al actualizar el Tema: " . $e->getMessage()];
         }
     }
 
-    public function DeleteModel($id) {
+
+
+
+    public function DeleteModel($id, $datos) {
+
         try {
             $conn = Conexion::Conexion();
             $stmt = $conn->prepare("
                 UPDATE titulos 
-                SET activo = false 
+                SET activo = false ,
+                fecha_actualizado = :fecha_actualizado
                 WHERE id_titulo = :id");
             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->bindParam(':fecha_actualizado', $datos['fecha_actualizado'], PDO::PARAM_STR);
             $stmt->execute();
             if ($stmt->rowCount() == 0) {
-                throw new Exception("No se encontró un título activo con ID $id");
+                return ['res' => false, 'data' => "No se encontro el titulo, intente mas tarde"];
             }
-            return "Título con ID $id desactivado exitosamente";
+            return ['res' => true, 'data' => "Titulo desactivado"];
         } catch (PDOException $e) {
-            throw new Exception("Error al desactivar el título: " . $e->getMessage());
+            return ['res' => false, 'data' => "Error al desactivar el titulo: " . $e->getMessage()];
         }
     }
+
+    public function ActivateModel($id, $datos) {
+
+        try {
+            $conn = Conexion::Conexion();
+            $stmt = $conn->prepare("
+                UPDATE titulos 
+                SET activo = true ,
+                fecha_actualizado = :fecha_actualizado
+                WHERE id_titulo = :id");
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->bindParam(':fecha_actualizado', $datos['fecha_actualizado'], PDO::PARAM_STR);
+            $stmt->execute();
+            if ($stmt->rowCount() == 0) {
+                return ['res' => false, 'data' => "No se encontro el titulo, intente mas tarde"];
+            }
+            return ['res' => true, 'data' => "Titulo desactivado"];
+        } catch (PDOException $e) {
+            return ['res' => false, 'data' => "Error al desactivar el titulo: " . $e->getMessage()];
+        }
+    }
+
+
+    private function adjustOrderTitulos($conn, $newOrder, $currentOrder, $idPunto) {
+        if ($newOrder < $currentOrder || $currentOrder == 0) {
+            // Incrementar orden para aquellos registros con `fk_titulos` que sean `NULL` y `id_punto` igual a `$idPunto`
+            $stmt = $conn->prepare("
+                UPDATE titulos 
+                SET orden_titulos = orden_titulos + 1 
+                WHERE orden_titulos >= :new_order 
+                  AND id_punto = :id_punto 
+                  AND fk_titulos IS NULL 
+                  AND activo = true
+            ");
+        }
+        $stmt->bindParam(':new_order', $newOrder, PDO::PARAM_INT);
+        $stmt->bindParam(':id_punto', $idPunto, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
+
+
+
 }
